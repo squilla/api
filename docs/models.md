d06f743d3a626f93717d2e4066f408ebe8450a09
Connor Cahill <connorcahill913@gmail.com>
2019-02-01 15:19:35 -0800

    finished setting up server, db, and config files. [NEXT]: ready to start creating resources(Users, Art).

---
+++ docs/models.md
@@ -0,0 +1,31 @@
+
+
+### Base Users 
+  - first name
+  - last name
+  - email
+  - password
+  - phone # (?)
+  - birthdate
+  
+### Reg User
+  - Favorite artists
+  
+### Artists 
+  - (inherit from user)
+  - artwork ---> arr of artwork id's
+  - feedback
+  - followers
+
+
+### Artwork
+  - title 
+  - image
+  - likes
+  - artist ---> single id to artist
+  - 
+
+
+### Feedback 
+  - type --> descripton, rating, describing word
+  - author ---> single id to user
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const app = require('./config/express');
+const config = require('./config/config');
 
 mongoose.Promise = Promise;
 
-//  Connect to Mongo db
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose.connect(
+  mongoUri,
+  { useNewUrlParser: true },
+);
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
 
+app.listen(config.port, () => console.log(`Running on port ${config.port}`)); // eslint-disable-line no-console
 
 module.exports = app;
