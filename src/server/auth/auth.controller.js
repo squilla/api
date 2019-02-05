6cbab68cb250aa5639d91508e020cb5907f99eb9
Connor Cahill <connorcahill913@gmail.com>
2019-02-05 11:17:01 -0800

    [Fixed]: refactored auth controller/routes to use async/await

--- src/server/auth/auth.controller.js
+++ src/server/auth/auth.controller.js
@@ -2,12 +2,6 @@ const jwt = require('jsonwebtoken');
 const Model = require('../user/user.model');
 const config = require('../../config/config');
 
-//  Creates new user
-function signUp(userData) {
-  const user = new Model.RegUser(userData);
-  // user._t = 'RegUser';
-  return user.save();
-}
 
 //  issues a cookie that authenticates user
 function issueCookie(res, user) {
@@ -16,31 +10,36 @@ function issueCookie(res, user) {
   res.status(200).send('User successfully signed in.');
 }
 
+//  Creates new user
+async function SignUp(req, res) {
+  const user = await new Model.RegUser(req.body);
+  await user.save();
+  return issueCookie(res, user);
+}
+
 //  Signs a user in using the compare password method
-function signIn(email, password, res) {
-  return Model.User.findOne({ email }, 'email password').then((user) => {
-    if (!user) {
-      return res.status(401).send('No user found with this email.');
+async function SignIn(req, res) {
+  const user = await Model.User.findOne({ email: req.body.email }, 'email password');
+  if (!user) {
+    return res.status(401).send('No user found with this email.');
+  }
+  //  User users compare password method to compare against hash
+  user.comparePassword(user.password, (err, isMatch) => {
+    if (!isMatch) {
+      return res.status(401).send('Email or password is incorrect.');
     }
-    //  compare the password with user method
-    user.comparePassword(password, (err, isMatch) => {
-      if (!isMatch) {
-        return res.status(401).send('Email or password is incorrect.');
-      }
-      return true;
-    });
-    return issueCookie(res, user);
-  }).catch(err => res.send(err));
+    return true;
+  });
+  return issueCookie(res, user);
 }
 
 //  Signs a user out (removes jwt token)
-function signOut(res) {
+function SignOut(res) {
   return res.clearCookie(config.cookie);
 }
 
 module.exports = {
-  signUp,
-  signIn,
-  signOut,
-  issueCookie,
+  SignUp,
+  SignIn,
+  SignOut,
 };
