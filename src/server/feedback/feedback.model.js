08db5741e18c17e4897ccbd4033b669f2b73fb02
Connor Cahill <connorcahill913@gmail.com>
2019-02-11 12:54:32 -0800

    [Added]: comment and reaction discriminators of feedback model.

--- src/server/feedback/feedback.model.js
+++ src/server/feedback/feedback.model.js
@@ -1,31 +1,26 @@
 const mongoose = require('mongoose');
 
-/*  ********
+const options = {
+  discriminatorKey: 'kind',
+  timestamps: true,
+};
+
+/*
  ! Chat with Dacio and see if is is necessary to setup like this
  *    FEEDBACK SCHEMA
  *  You can look in user.model for an example of discriminators
  *   There is 1 main feedback model/schema that all other feedback inherit from.
  *  The discriminators of feedback (sub schemas) that all inherit from Feedback
  *  Discriminators of Feedback Model:
- *  - comments
+ *  - comments ---done
  *  - critiques
  *  - ratings
- *  - reaction
- *
- *
+ *  - reaction --- done
  */
-
 
 const Schema = mongoose.Schema; // eslint-disable-line
 
 const FeedbackSchema = new Schema({
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  updatedAt: {
-    type: Date,
-  },
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User',
@@ -40,8 +35,56 @@ const FeedbackSchema = new Schema({
     type: String,
     required: true,
   },
-});
+}, options);
 
 const Feedback = mongoose.model('Feedback', FeedbackSchema);
 
-module.exports = Feedback;
+//* Comments are feedback type where user types in a response
+const CommentSchema = new Schema({
+  content: {
+    type: String,
+    trim: true,
+  },
+}, options);
+
+// requireCom function makes the content required if feedback type is comment
+function requireCom() {
+  return this.feedbackType === 'comment';
+}
+//  makes these fields true
+CommentSchema.path('content').required(requireCom());
+
+//  Creates comment discriminator of Feedback model
+const Comment = Feedback.discriminator('Comment', CommentSchema);
+
+//* Reactions are when users pick one of the predefined 'reactions'/ratings
+const ReactionSchema = new Schema({
+  reaction: {
+    type: String,
+  },
+  sentiment: {
+    type: String,
+  },
+}, options);
+
+// requireCom function makes the content required if feedback type is comment
+function requireReaction() {
+  return this.feedbackType === 'reaction';
+}
+//  makes these fields true
+ReactionSchema.path('reaction').required(requireReaction());
+
+
+//! Need to setup logic for sentiment with reactions.
+//! Discuss at meeting
+// ReactionSchema.pre('save', () => {
+
+// })
+
+const Reaction = Feedback.discriminator('Reaction', ReactionSchema);
+
+module.exports = {
+  Feedback,
+  Comment,
+  Reaction,
+};
