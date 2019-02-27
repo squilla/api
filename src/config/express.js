33f31421c8f5402288fc9840b8bd80e09425aece
Connor Cahill <connorcahill913@gmail.com>
2019-02-27 11:38:32 -0800

    [Added]: Cors middleware in express config

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const routes = require('../index.routes');
 const checkAuth = require('../middleware/checkAuth');
 
 const app = express();
 
 //  Setup middleware:
+app.use(cors());
 app.use(cookieParser());
 app.use(checkAuth);
 app.use(express.json());
