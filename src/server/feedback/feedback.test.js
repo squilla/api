1e4b8ffd16f6e8c539e997b402968c4eb8dd2801
Connor Cahill <connorcahill913@gmail.com>
2019-02-28 16:25:08 -0800

    [fixing]: tests

--- src/server/feedback/feedback.test.js
+++ src/server/feedback/feedback.test.js
@@ -1,5 +1,6 @@
 const sinon = require('sinon');
 const sinonTest = require('sinon-test');
+require('sinon-mongoose');
 
 const Model = require('./feedback.model');
 const commentController = require('./types/comment.controller');
@@ -10,18 +11,25 @@ require('../../config/config');
 const test = sinonTest(sinon);
 
 describe('Feedback', () => {
-  const feedback = {
-    _id: 50,
-    art: 49,
-    feedbackType: 'comment',
-    content: 'This is a test comment',
-  };
-  const reaction = {
-    _id: 55,
-    art: 56,
-    feedbackType: 'reaction',
-    reaction: 'Happy',
-  };
+  let feedback;
+  let reaction;
+
+  beforeEach(() => {
+    feedback = {
+      _id: 50,
+      art: 49,
+      feedbackType: 'comment',
+      content: 'This is a test comment',
+    };
+
+    reaction = {
+      _id: 55,
+      art: 56,
+      feedbackType: 'reaction',
+      reaction: 'Happy',
+    };
+  });
+
 
   it('Should INDEX all feedback (regardless of types at GET: /api/feedback', test(async function indexTest() {
     this.stub(Model.Feedback, 'find').resolves([feedback]);
@@ -40,7 +48,7 @@ describe('Feedback', () => {
 
   it('Should INDEX all Comment feedback types at GET: /api/feedback/comments', test(async function testCommentIndex() {
     this.stub(Model.Comment, 'find').resolves([feedback]);
-
+    
     const req = {};
     const res = {
       send: this.spy(),
@@ -101,7 +109,7 @@ describe('Feedback', () => {
 
     await feedbackController.Create(req, res);
 
-    sinon.assert.calledOnce(Model.Feedback.Create);
+    sinon.assert.calledOnce(Model.Feedback);
     sinon.assert.calledOnce(res.send);
   }));
 });
